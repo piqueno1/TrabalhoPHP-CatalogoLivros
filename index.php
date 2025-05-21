57b928d5eb8aaea505d34893fc1831974bdd44d2
Felipe <164506461+piqueno1@users.noreply.github.com>
2025-05-20 21:33:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catálogo de Livros</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="menu">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="livros.php">Cadastrar Livro</a></li>
+                <li><a href="listas.php">Lista de Livros</a></li>
+            </ul>
+        </nav>
+    </header>
+    <footer>direitos reservados</footer>
+</body>
+</html>
